2fd936c0d0891e17110247d3d5ce91365dc062c2
yubingcen <644268318@qq.com>
2019-11-20 14:07:34 +0800

    引入中间件

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
 import Koa from 'koa'
-import router from "./routes/route";
+import route from "./routes/route";
 import helmet from 'koa-helmet' //安全头部
+import koaBody from 'koa-body'
+import koaCors from "koa-cors" // 解决跨域
 import { Context } from "koa"
 import { Port, Mongodb } from './config/configs'
 import mongoose from 'mongoose'
 const app = new Koa()
 
+app.use(koaBody({
+  multipart: true,
+  formidable: {
+    hash: 'md5',
+    maxFileSize: 20 * 1024 * 1024 // 设置上传文件大小最大限制，默认20M
+  }
+}))
+
 app.use(helmet())
-app.use(router())
+app.use(koaCors())
+app.use(route())
 app.use(async (ctx: Context) => {
 
   ctx.body = 'Hello World!'
 });
+
 
 mongoose.connect(Mongodb, { useNewUrlParser: true,  useUnifiedTopology: true }, (err) => {
   if (err) {
