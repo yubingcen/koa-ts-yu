8f6797df48a26721dff673d3194d8fd06e822bd0
yubingcen <644268318@qq.com>
2019-11-28 14:13:52 +0800

    redis基本

---
+++ src/config/redisConfig.ts
@@ -0,0 +1,55 @@
+import redis from 'redis'
+// import { promisifyAll } from 'bluebird'
+const options: any = {
+  host: '192.168.0.107',
+  port: 15001,
+  password: '123456',
+  detect_buffers: true,
+  retry_strategy: (options: any) => {
+    if (options.error && options.error.code === 'ECONNREFUSED') {
+      // End reconnecting on a specific error and flush all commands with
+      // a individual error
+      return new Error('The server refused the connection');
+    }
+    if (options.total_retry_time > 1000 * 60 * 60) {
+      // End reconnecting after a specific timeout and flush all commands
+      // with a individual error
+      return new Error('Retry time exhausted');
+    }
+    if (options.attempt > 10) {
+      // End reconnecting with built in error
+      return undefined;
+    }
+    // reconnect after
+    return Math.min(options.attempt * 100, 3000);
+  }
+}
+
+const client = redis.createClient(options)
+// const client = promisifyAll(redis.createClient(options))
+
+const setValue = (key: string, value: any) => {
+  if (typeof value === 'undefined' || value === null || value === '') {
+    return
+  }
+  if (typeof value === 'string') {
+    client.set(key, value)
+  } else if (typeof value === 'object') {
+    Object.keys(value).forEach(item => {
+      client.hset(key, item, value[item], redis.print)
+    })
+  }
+}
+
+import { promisify } from 'util'
+const getAsync = promisify(client.get).bind(client)
+
+const getValue = (key: string) => {
+  return getAsync(key)
+}
+
+export {
+  client,
+  setValue,
+  getValue
+}
