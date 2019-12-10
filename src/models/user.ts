ce7990e7c47fd7df77028e6071240d259d332413
yubingcen <644268318@qq.com>
2019-12-10 18:30:52 +0800

    model user

---
+++ src/models/user.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+const UserSchema = new Schema({
+  userId: {
+    type: String,
+    unique: true,
+    require: true
+  },
+  account: {
+    type: String
+  },
+  username: {
+    type: String
+  },
+  nickname: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  mobile: {
+    type: String
+  },
+  headerImg: {
+    type: String
+  },
+  created: {
+    type: String
+  },
+  rolls: {
+    type: Array,
+    default: ['user']
+  }
+}, { collection: 'user', versionKey: false })
+
+export default mongoose.model('user', UserSchema)
