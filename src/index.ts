5ea221c475e7a8ffcd720f8d3f697afc1ea058bb
yubingcen <644268318@qq.com>
2019-12-10 18:48:16 +0800

    use koa-compose

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,31 @@ import helmet from 'koa-helmet' //安全头部
 import koaBody from 'koa-body'
 import koaCors from "koa-cors" // 解决跨域
 import jwt from 'koa-jwt'
+import compose from 'koa-compose'
 import CONFIG from './config/configs'
 import ErrorHandle from './common/errorHandle'
 const app = new Koa()
 
-app.use(koaBody({
-  multipart: true,
-  formidable: {
-    hash: 'md5',
-    maxFileSize: 20 * 1024 * 1024 // 设置上传文件大小最大限制，默认20M
-  }
-}))
-app.use(ErrorHandle)
-app.use(jwt({secret: CONFIG.SECRET}).unless({ path: [/^\/public\/\S*/, /\/login/, /\/register/]}))
-app.use(helmet())
-app.use(koaCors())
+const JWT = jwt({secret: CONFIG.SECRET}).unless({ path: [/^\/public\/\S*/, /\/login/, /\/register/]})
+
+/**
+ * 使用koa-compose 集成中间件
+ */
+const middleware = compose([
+  koaBody({
+    multipart: true,
+    formidable: {
+      hash: 'md5',
+      maxFileSize: 20 * 1024 * 1024 // 设置上传文件大小最大限制，默认20M
+    }
+  }),
+  koaCors(),
+  helmet(),
+  ErrorHandle,
+  JWT
+])
+
+app.use(middleware)
 app.use(route())
 
 app.listen(CONFIG.PORT)
